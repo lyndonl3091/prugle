f42a7f1ce053ac7606a1c3b0ee27e49054cd6642
lyndonl3091 <lyndonl3091@gmail.com>
2019-02-09 14:02:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// import SplashPage from './components/SplashPage/SplashPage';
+// import Aux from './hoc/Aux';
+// import Nav from './components/common/Navbar';
+
+export class App extends Component {
+
+  render() {
+
+
+    return (
+
+        <div>Test</div>
+    )
+  }
+
+
+}
+
+export default App;
