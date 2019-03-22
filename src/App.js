c4240b9388e6ee977d9a6c5d99f8584403637553
lyndonl3091 <lyndonl3091@gmail.com>
2019-03-21 21:30:08 -0400

    initial client

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// import Aux from './hoc/Aux';
+// import Nav from './components/common/Navbar';
+
+export class App extends Component {
+
+  render() {
+
+
+    return (
+
+        <div>Test</div>
+    )
+  }
+
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import createSagaMiddleware from 'redux-saga'
+
+import App from './App';
+import SignUp from './components/auth/SignUp';
+import reducers from './reducers';
+import { toHome, toLogin, toSignUp } from './routePaths';
+
+const sagaMiddleware = createSagaMiddleware()
+
+const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router>
+        <Switch>
+          <Route exact path={toHome} component={App} />
+          <Route path={toSignUp} component={SignUp} />
+        </Switch>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>
+  , document.getElementById('root'));
