dd003c4f7e0c15eded7125661b5e902eb97d670c
lyndonl3091 <lyndonl3091@gmail.com>
2019-03-29 23:25:07 -0400

    actions reducers'

---
+++ src/actions/types.js
@@ -0,0 +1,12 @@
+export const GET_DEALS = 'GET_DEALS'
+
+export const SIGN_UP = 'SIGN_UP'
+export const LOG_IN = 'LOG_IN'
+export const LOG_OUT = 'LOG_OUT'
+export const AUTH_SUCCESSFUL = 'AUTH_SUCCESSFUL'
+export const AUTH_FAILED = 'AUTH_FAILED'
+export const AUTH_START = 'AUTH_START'
+
+export const AUTH_USER = 'AUTH_USER'
+export const UNAUTH_USER = 'UNAUTH_USER'
+export const AUTH_ERROR = 'AUTH_ERROR'
