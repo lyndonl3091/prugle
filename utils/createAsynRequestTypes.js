b9c26f483552ea9f11b8c772bab458f036a3520f
lyndonl3091 <lyndonl3091@gmail.com>
2019-04-06 11:57:03 -0400

    create types

--- utils/createAsynRequestTypes.js
+++ utils/createAsynRequestTypes.js
@@ -1,6 +1,15 @@
 const types = ['TRY', 'SUCCESS', 'FAILURE']
 
 
-export default function(val) {
-    let 
+const types = ['TRY', 'SUCCESS', 'FAILURE']
+
+export default function createAsynRequestTypes(val) {
+	return types.reduce((acc, next) => {
+  	acc = {
+      	...acc,
+        [next]: `${val}.${next}`
+      }
+      
+    return acc
+  }, {})
 }
