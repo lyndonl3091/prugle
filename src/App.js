98a46d6ce01d9e808257daf579ebf1649bdaf50c
lyndonl3091 <lyndonl3091@gmail.com>
2019-06-27 20:38:52 -0400

    app bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import SplashPage from 'components/SplashPage'
 // import Aux from './hoc/Aux';
 // import Nav from './components/common/Navbar';
+import AppBar from './components/common/AppBar'
 
 export class App extends Component {
   render() {
     return (
-      <SplashPage />
+      <Fragment>
+        <AppBar />
+        <SplashPage />
+      </Fragment>
     )
   }
 }
