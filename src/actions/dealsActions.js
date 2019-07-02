ff926dc2a27a5641212a7c31289aca66785508a3
lyndonl3091 <lyndonl3091@gmail.com>
2019-07-02 03:51:25 -0400

    restructured actions

--- src/actions/dealsActions.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncActions } from 'utils'
-
-import * as TYPES from './types'
-
-export const getDeals = createAsyncActions(TYPES.GET_DEALS)
-
-export const addDeal = createAsyncActions(TYPES.ADD_DEAL)
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,7 @@
-export * from './authActions'
-export * from './dealsActions'
+import { createAsyncActions } from 'utils'
+
+import * as TYPES from './types'
+
+export const getDeals = createAsyncActions(TYPES.GET_DEALS)
+
+export const addDeal = createAsyncActions(TYPES.ADD_DEAL)
