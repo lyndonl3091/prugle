d345b34cc3f82dabff84769306099f80d055e03f
lyndonl3091 <lyndonl3091@gmail.com>
2019-08-23 07:58:45 -0400

    common styled components

---
+++ src/styles/common.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const fieldStyle = {
+  wrapper: { display: 'flex', justifyContent: 'center'},
+  node: { flex: 1 }
+}
+
+export const SplashWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ColumnFlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+`
+
+export const FlexWrapper = styled.div`
+  flex: 1;
+  width: 100%;
+`
