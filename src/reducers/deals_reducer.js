6ff777e7cb90094b92d71495906cddb49a535ee2
lyndonl3091 <lyndonl3091@gmail.com>
2019-10-21 11:14:19 -0400

    cleanup, flow init

--- src/reducers/deals_reducer.js
+++ src/reducers/deals_reducer.js
@@ -3,14 +3,14 @@ import { fromJS, setIn } from 'immutable'
 import initialState from './initialState'
 
 
-export default function(state = initialState, action) {
-    switch(action.type) {
+export default function(state = initialState, { type, payload}) {
+    switch(type) {
         case types.GET_DEALS.SUCCESS:
             let result = state
-            const { deals } = action.payload
+            const { deals } = payload
             result = result.setIn(['listOfDeals'], fromJS(deals))
             return result
         default:
         return state
     }
-}   
+}
---
+++ src/views/Deals/Deal.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Deal = props => (
+  <div>Deal #1</div>
+)
+
+export default Deal
