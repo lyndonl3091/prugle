a1c1c754480ac72561846ff175233f01f11ee674
lyndonl3091 <lyndonl3091@gmail.com>
2019-11-11 19:27:38 -0500

    removed form

--- src/views/Deals/AddDeal.js
+++ src/views/Deals/AddDeal.js
@@ -2,62 +2,70 @@ import React, { Component, Fragment, useState } from 'react'
 import { connect } from 'react-redux'
 import { TextField, RaisedButton } from '@material-ui/core'
 import { addDeal } from 'actions'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 
 const mapDispatchToProps = dispatch => ({
     addDealActon: deal => dispatch(addDeal.try(deal))
 })
 
+// export const AddDeal = props => {
+//     const [title, setTitle] = useState('')
+//     const [description, setDescription] = useState('')
+//     const [link, setLink] = useState('')
+
+//     function handleSubmit() {
+//         const { addDealAction } = props
+//         addDealAction({
+//            title,
+//            description,
+//            link
+//         })
+//     }
+
+//     function inputTitle(e) {
+//         const val = e.target.value
+//         setTitle(val)
+//     }
+
+//     function inputDescription(e) {
+//         const val = e.target.value
+//         setDescription(val)
+//     }
+
+//     function inputLink(e) {
+//         const val = e.target.value
+//         setLink(val)
+//     }
+
+//     return (
+//         <>
+//         <form onSubmit={handleSubmit}>
+
+//             <TextField
+//                 label="Title"
+//                 onChange={inputTitle}
+//             />
+//             <br/>
+//             <TextField
+//                 label="Description"
+//                 onChange={inputDescription}
+//             />
+//             <br/>
+//             <TextField
+//                 label="Link"
+//                 onChange={inputLink}
+//             />
+//             <br/>
+//         </form>
+//     </>
+//     )
+// }
+
 export const AddDeal = props => {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [link, setLink] = useState('')
-
-    function handleSubmit() {
-        const { addDealAction } = props
-        addDealAction({
-           title,
-           description,
-           link
-        })
-    }
-
-    function inputTitle(e) {
-        const val = e.target.value
-        setTitle(val)
-    }
-
-    function inputDescription(e) {
-        const val = e.target.value
-        setDescription(val)
-    }
-
-    function inputLink(e) {
-        const val = e.target.value
-        setLink(val)
-    }
 
     return (
-        <>
-        <form onSubmit={handleSubmit}>
-
-            <TextField
-                label="Title"
-                onChange={inputTitle}
-            />
-            <br/>
-            <TextField
-                label="Description"
-                onChange={inputDescription}
-            />
-            <br/>
-            <TextField
-                label="Link"
-                onChange={inputLink}
-            />
-            <br/>
-        </form>
-    </>
+        <div>Test</div>
     )
 }
 
