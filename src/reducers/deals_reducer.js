5fca4a1168e4d1d469e2f10871c156317d95d3fe
lyndonl3091 <lyndonl3091@gmail.com>
2020-03-15 19:22:15 -0400

    behaviors map

--- src/reducers/deals_reducer.js
+++ src/reducers/deals_reducer.js
@@ -1,23 +1,40 @@
-import * as types from 'types'
+import * as CONSTANTS from 'types'
 import { fromJS, setIn } from 'immutable'
 import initialState from './initialState'
 
-
-export default function(state = initialState, { type, payload}) {
-    switch(type) {
-        case types.GET_DEALS.REQUEST:
-            return state
-        case types.GET_DEALS.FAILURE:
-            return state            
-        case types.GET_DEALS.SUCCESS:
-            let result = state
-            const { deals } = payload
-            result = result.setIn(['listOfDeals'], fromJS(deals))
-            return result
-        case types.ADD_DEAL.SUCCESS:
-            return state
-            
-        default:
+const behaviors = {
+    [CONSTANTS.GET_DEALS.REQUEST]: (state, payload) => {
         return state
+    },
+    [CONSTANTS.GET_DEALS.SUCCESS]: (state, payload) => {
+        let result = state
+        const { deals } = payload
+
+        result = result.setIn(['listOfDeals'], fromJS(deals))
+        
+        return result
     }
 }
+
+export default function(state = initialState, { type, payload }) {
+    return behaviors[type](state, payload)
+}
+
+
+// export default function(state = initialState, { type, payload }) {
+//     switch(type) {
+//         case types.GET_DEALS.REQUEST:
+//             return state
+//         case types.GET_DEALS.FAILURE:
+//             return state            
+//         case types.GET_DEALS.SUCCESS:
+//             let result = state
+//             const { deals } = payload
+//             result = result.setIn(['listOfDeals'], fromJS(deals))
+//             return result
+//         case types.ADD_DEAL.SUCCESS:
+//             return state
+//         default:
+//         return state
+//     }
+// }
